ff4c6e16761d953c0af6d9a50554a868ac912683
Chris Wall <cwall1@nhs.net>
2016-02-10 10:10:02 +0000

    moved upload directory

--- app/views.py
+++ app/views.py
@@ -17,15 +17,16 @@ celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 @celery.task()
-def query_zippy(file):
+def query_zippy(uploadFile):
     # some long running task here
-	# filename = secure_filename(file.filename)
-	# print filename
-	# file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-	# print "file saved to ./uploads/%s" % filename
-	# os.chdir('./app/')
-	# print subprocess.call(['./zippy.py', 'get', '--outfile', outfile, '../uploads/%s'% filename], shell=False)
-	return "running zippy..."
+    print >> sys.stderr, dir(uploadFile)
+    filename = secure_filename(uploadFile.filename)
+    print filename
+    uploadFile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    print "file saved to ./uploads/%s" % filename
+    os.chdir('./app/')
+    print subprocess.call(['./zippy.py', 'get', '--outfile', outfile, '../uploads/%s'% filename], shell=False)
+    print "running zippy..."
 
 
 def allowed_file(filename):
@@ -35,45 +36,46 @@ def allowed_file(filename):
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/no_file')
 def no_file():
-	return render_template('no_file.html')
+    return render_template('no_file.html')
 
 @app.route('/file_uploaded')
 def file_uploaded():
-	return render_template('file_uploaded.html')
-
-# @app.route('/upload/', methods=['POST'])
-# def upload():
-# 	file = request.files['filePath']
-# 	print("request of file successful")
-# 	if file and allowed_file(file.filename):
-# 		filename = secure_filename(file.filename)
-# 		print filename
-# 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-# 		print "file saved to ./uploads/%s" % filename
-# 		os.chdir('./app/')
-# 		print subprocess.call(['./zippy.py', 'get', '--outfile', 'outfile', '../uploads/%s'% filename], shell=False)
-# 		# print os.system('./zippy.py batch ././uploads/%s'% filename)'./zippy.py get -o', outfile, '../uploads/%s'% filename
-# 		# print list_content.communicate()
-# 		print "running zippy..."
-# 		return redirect('/file_uploaded')
-# 	else:
-# 		print("file for upload not supplied or file-type not allowed")
-# 		return redirect('/no_file')
+    return render_template('file_uploaded.html')
 
 @app.route('/upload/', methods=['POST'])
 def upload():
-	ourFile = request.files['filePath']
-	print("request of file successful")
-	if ourFile and allowed_file(ourFile.filename):
-		query_zippy.apply_async(args=[ourFile])
-		return redirect('/file_uploaded')
-	else:
-		print("file for upload not supplied or file-type not allowed")
-		return redirect('/no_file')
+  uploadFile = request.files['filePath']
+  print("request of file successful")
+  if uploadFile and allowed_file(uploadFile.filename):
+      filename = secure_filename(uploadFile.filename)
+      print filename
+      print app.config['UPLOAD_FOLDER']
+      uploadFile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      print "file saved to ./uploads/%s" % filename
+      os.chdir('./app/')
+      print subprocess.call(['./zippy.py', 'get', '--outfile', 'outfile', '../uploads/%s'% filename], shell=False)
+      # print os.system('./zippy.py batch ././uploads/%s'% filename)'./zippy.py get -o', outfile, '../uploads/%s'% filename
+      # print list_content.communicate()
+      print "running zippy..."
+      return redirect('/file_uploaded')
+  else:
+      print("file for upload not supplied or file-type not allowed")
+      return redirect('/no_file')
+
+# @app.route('/upload/', methods=['POST'])
+# def upload():
+#     ourFile = request.files['filePath']
+#     print("request of file successful")
+#     if ourFile and allowed_file(ourFile.filename):
+#         query_zippy.apply_async(args=[ourFile])
+#         return redirect('/file_uploaded')
+#     else:
+#         print("file for upload not supplied or file-type not allowed")
+#         return redirect('/no_file')
 
 
 
